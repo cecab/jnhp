017e006c9bff8acca18ff6b6bc5982a9654d86be
Cesar Castillo <cesar.castillo@compulinux.com>
2005-11-04 22:25:45 -0500

    The interface for runApplication, some libs as binary(TODO) and minor files for eclipse/IDE

---
+++ compile.sh
@@ -0,0 +1,1 @@
+javac -cp lib/log4j.jar:lib/servlet-api.jar:lib/commons-logging-1.1.3.jar -Xlint:unchecked -d . `find src/ -name \*java ` 
---
+++ src/com/compulinux/jnhp/runApplication.java
@@ -0,0 +1,11 @@
+package com.compulinux.jnhp ; 
+/**
+ * @author ccastill
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public interface runApplication {
+	public void runApp(Jnhp html) throws Exception;
+
+}
